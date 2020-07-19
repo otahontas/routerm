ea8d5a52309c7d1e11dad2ec89aa8aff4f51e127
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-07-19 20:11:16 +0300

    Add function for getting geolocation by address from api

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,17 @@ fn get_api_key() -> String {
     }
 }
 
+async fn get_geolocation(address: &str, api_key: String) -> Result<String, Box<dyn std::error::Error>> {
+    let url = format!("https://api.openrouteservice.org/geocode/search?api_key={}&text={}", api_key, address);
+    let resp = reqwest::get(&url)
+        .await?
+        .text()
+        .await?;
+
+    Ok(resp)
+}
+
 #[tokio::main]
 async fn main() {
-
-
+    let api_key = get_api_key();
 }
