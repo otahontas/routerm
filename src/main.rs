442ec7bbf6494c473365c7f555174f4d863150e1
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-07-19 20:18:12 +0300

    Add geolocation parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use dotenv;
 use std::env;
 use std::process;
+use geojson::GeoJson;
+use geojson::Value::Point;
 
 fn get_api_key() -> String {
     dotenv::dotenv().ok();
@@ -20,8 +22,18 @@ async fn get_geolocation(address: &str, api_key: String) -> Result<String, Box<d
         .await?
         .text()
         .await?;
-
     Ok(resp)
+}
+
+fn parse_geolocation(geo_result_string: &str)  -> Option<(f64, f64)> {
+    if let GeoJson::FeatureCollection(parsed_geo_result) = geo_result_string.parse::<GeoJson>().unwrap() {
+        if let Some(geometry) = &parsed_geo_result.features[0].geometry {
+            if let Point(point)  = &geometry.value {
+                return Some((point[0], point[1]));
+            }
+        }
+    }
+    None
 }
 
 #[tokio::main]
