8d15541b7fb6798b057930515c66ee6be4e2b664
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-07-19 20:07:18 +0300

    Add api_key check, api_key should be set as env variable
    User can add api_key to .env if wanted.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use dotenv;
+use std::env;
+use std::process;
+
+fn get_api_key() -> String {
+    dotenv::dotenv().ok();
+    let key = "ROUTESERVICE_API_KEY";
+    match env::var(key) {
+        Ok(val) => return val,
+        Err(e) => {
+            println!("You should define {}. Following error happened: {}", key, e);
+            process::exit(1);
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
+
 
 }
